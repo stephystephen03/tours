d9c67a2c4279e853e5df3f1e0369bd0ba00dc255
stephy stephen <stephystephen03@gmail.com>
2023-06-29 13:27:55 +1000

    Implement refetch functionality by adding a button

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,57 @@ const url = "https://course-api.com/react-tours-project";
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [tours, setTours] = useState([]);
-  const removeTour = (id) =>{
-    const newTours = tours.filter((tour) => tour.id !== id)
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
     setTours(newTours);
-  }
-  useEffect(() => {
-    const fetchTours = async () => {
-      setIsLoading(true)
-      try {
-        const response = await fetch(url);
-        const tours = await response.json();
-        setTours(tours);
-        console.log(tours);
-      } catch (error) {
-        console.log(error);
-      }
-      setIsLoading(false)
+  };
 
-    };
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+      console.log(tours);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
     fetchTours();
   }, []);
-  {isLoading && <main><Loading/></main>}
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button
+            className="btn"
+            style={{ marginTop: "2rem" }}
+            type="button"
+            onClick={() => fetchTours()}
+          >
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
       {/* {tours && <Loading />} */}
-      <Tours tours={tours} removeTour={removeTour}/>
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 };
--- src/Loading.jsx
+++ src/Loading.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Loading = () => {
   return (
     <div className='loading'>
-      Loading...
     </div>
   )
 }
